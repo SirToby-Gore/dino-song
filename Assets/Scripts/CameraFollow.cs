6e13cdea4f499e9f9453091ebb72136fcdbb05bd
Toby Gore <tobyedwardgore+git@gmail.com>
2026-03-11 12:02:30 +0000

    Adding Camera Scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class IsVisible : MonoBehaviour
+{
+    Renderer m_Renderer;
+    public Camera main_camera;
+    // Use this for initialization
+
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Renderer.isVisible)
+        {
+            Debug.Log("Object is visible");
+        }
+        else 
+        {
+            Debug.Log("Object is no longer visible");
+            Vector3 pos = main_camera.transform.position;
+            pos.y += 17f;
+            main_camera.transform.position = pos;
+        }
+    }
+}
