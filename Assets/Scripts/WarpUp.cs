5906dd0398d64d72d6d6f79b1be9636a49b1018e
BGrratty <bengarratt07@gmail.com>
2026-03-11 15:48:10 +0000

    added warping

---
+++ Assets/Scripts/WarpUp.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class WarpUp : MonoBehaviour
+{
+    public Rigidbody2D player_body;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("COLLISION ACTIVE");
+            Vector3 pos = player_body.transform.position;
+            pos.y += 15f;
+            player_body.transform.position = pos;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
